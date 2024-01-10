b797669a5c6baf516b6f548b2eb04b3c99407fd2
divyansh <divyanshp1136@gmail.com>
2024-01-10 19:39:32 +0530

    preprocess

--- detect_hand.py
+++ detect_hand.py
@@ -1,17 +1,18 @@
 import mediapipe as mp 
 import cv2
+from preprocess import preprocess_data
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
-
-
 
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
+
+  
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -28,6 +29,8 @@ with mp_hands.Hands(
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
         mp_drawing.draw_landmarks(
@@ -36,8 +39,14 @@ with mp_hands.Hands(
             mp_hands.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
-    # Flip the image horizontally for a selfie-view display.
+
+    # print(type(results.multi_hand_landmarks))
+    
+    preprocess_data(results)
+
     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
+
+
 cap.release()
---
+++ preprocess.py
@@ -0,0 +1,10 @@
+import mediapipe as mp
+import cv2
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+def preprocess_data(result):
+
+    print(result.multi_hand_landmarks)
